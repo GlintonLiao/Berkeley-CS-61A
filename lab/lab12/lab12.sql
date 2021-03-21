7e44a593a57ee2b39b3db8013ed23a6208432a1d
Guotong Liao <37015336+GlintonLiao@users.noreply.github.com>
2021-03-21 11:31:36 +0800

    Update
    Labs, projects, homework for CS 61A

---
+++ lab/lab12/lab12.sql
@@ -0,0 +1,21 @@
+.read data.sql
+
+
+CREATE TABLE bluedog AS
+  SELECT color, pet FROM students WHERE color = 'blue' AND pet = 'dog';
+
+CREATE TABLE bluedog_songs AS
+  SELECT color, pet, song FROM students WHERE color = 'blue' AND pet = 'dog';
+
+
+CREATE TABLE smallest_int AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE matchmaker AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
+
+CREATE TABLE sevens AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+
